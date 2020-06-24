1e92d74a0a57b88b13857c30d2e92f724beeda54
marceau-maubert <marceau.maubert2@gmail.com>
2020-06-24 22:05:49 +0200

    This is how you should do this

--- lua/metaconcord/init.lua
+++ lua/metaconcord/init.lua
@@ -1,7 +1,7 @@
 require("gwsockets")
 
 metaconcord = metaconcord or {
-    payloads = {}
+	payloads = {}
 }
 
 local token = file.Read("metaconcord-token.txt", "DATA")
@@ -10,127 +10,128 @@ local path = "metaconcord/payloads/%s"
 local headerCol = Color(53, 219, 166)
 
 function metaconcord.print(...)
-    MsgC(headerCol, "[metaconcord] ", Color(255, 255, 255), ...)
-    Msg("\n")
+	MsgC(headerCol, "[metaconcord] ", Color(255, 255, 255), ...)
+	Msg("\n")
 end
 
 function metaconcord.connect()
-    if metaconcord.socket and metaconcord.socket:isConnected() then return end -- metaconcord.print("Already connected!")
-    local socket = GWSockets.createWebSocket(("ws://%s"):format(endpoint))
-    socket:setHeader("X-Auth-Token", token)
+	if metaconcord.socket and metaconcord.socket:isConnected() then return end -- metaconcord.print("Already connected!")
+	local socket = GWSockets.createWebSocket(("ws://%s"):format(endpoint))
+	socket:setHeader("X-Auth-Token", token)
 
-    function socket:onMessage(data)
-        data = util.JSONToTable(data)
+	function socket:onMessage(data)
+		data = util.JSONToTable(data)
 
-        if data then
-            for _, payload in next, metaconcord.payloads do
-                if data.payload.name == payload.name then
-                    payload:handle(data.payload)
-                end
-            end
-        end
-    end
+		if data then
+			for _, payload in next, metaconcord.payloads do
+				if data.payload.name == payload.name then
+					payload:handle(data.payload)
+				end
+			end
+		end
+	end
 
-    function socket:onError(err)
-        metaconcord.print("Error: ", Color(255, 0, 0), err)
-        metaconcord.disconnect()
-    end
+	function socket:onError(err)
+		metaconcord.print("Error: ", Color(255, 0, 0), err)
+		metaconcord.disconnect()
+	end
 
-    function socket:onConnected()
-        metaconcord.print("Connected.")
+	function socket:onConnected()
+		metaconcord.print("Connected.")
 
-        for _, script in next, (file.Find(path:format("*.lua"), "LUA")) do
-            local name = string.StripExtension(script)
+		for _, script in next, (file.Find(path:format("*.lua"), "LUA")) do
+			local name = string.StripExtension(script)
 
-            if name ~= "Payload" then
-                local Payload = include(path:format(script))
-                metaconcord.payloads[Payload.name] = Payload(self)
-            end
-        end
+			if name ~= "Payload" then
+				local Payload = include(path:format(script))
+				metaconcord.payloads[Payload.name] = Payload(self)
+			end
+		end
 
-        for _, folder in next, ({file.Find(path:format("*"), "LUA")})[2] do
-            local scriptPath = (path .. "/%s"):format(folder, "init.lua")
-            local clScriptPath = (path .. "/%s"):format(folder, "cl_init.lua")
+		for _, folder in next, ({file.Find(path:format("*"), "LUA")})[2] do
+			local scriptPath = (path .. "/%s"):format(folder, "init.lua")
+			local clScriptPath = (path .. "/%s"):format(folder, "cl_init.lua")
 
-            if file.Exists(scriptPath, "LUA") then
-                local Payload = include(scriptPath)
-                metaconcord.payloads[Payload.name] = Payload(self)
-            end
+			if file.Exists(scriptPath, "LUA") then
+				local Payload = include(scriptPath)
+				metaconcord.payloads[Payload.name] = Payload(self)
+			end
 
-            if file.Exists(clScriptPath, "LUA") then
-                AddCSLuaFile(clScriptPath)
-            end
-        end
+			if file.Exists(clScriptPath, "LUA") then
+				AddCSLuaFile(clScriptPath)
+			end
+		end
 
-        timer.Simple(0, function()
-            for _, payload in next, metaconcord.payloads do
-                if payload.onConnected then
-                    payload:onConnected()
-                end
-            end
-        end)
-    end
+		timer.Simple(0, function()
+			for _, payload in next, metaconcord.payloads do
+				if payload.onConnected then
+					payload:onConnected()
+				end
+			end
+		end)
+	end
 
-    function socket:onDisconnected()
-        for k, payload in pairs(metaconcord.payloads) do
-            payload:__gc() -- 5.2 only :(
-            metaconcord.payloads[k] = nil
-        end
-    end
+	function socket:onDisconnected()
+		for k, payload in pairs(metaconcord.payloads) do
+			payload:__gc() -- 5.2 only :(
+			metaconcord.payloads[k] = nil
+		end
 
-    metaconcord.socket = socket
-    metaconcord.print("Connecting...")
-    metaconcord.socket:open()
+		self = nil
+		metaconcord.print("Disconnected.")
+	end
 
-    timer.Create("metaconcordHeartbeat", 10, 0, function()
-        if metaconcord.socket and metaconcord.socket:isConnected() then
-            metaconcord.socket:write("") -- heartbeat LOL
-        else
-            metaconcord.print("Lost connection, reconnecting...")
-            metaconcord.start()
-        end
-    end)
+	metaconcord.print("Connecting...")
+	socket:open()
+	metaconcord.socket = socket
 end
 
 function metaconcord.disconnect()
-    if not metaconcord.socket or not metaconcord.socket:isConnected() then return end -- metaconcord.print("Not connected.")
-    metaconcord.print("Disconnecting...")
-    metaconcord.socket:close()
-    metaconcord.socket = nil
-    metaconcord.print("Disconnected.")
+	if not metaconcord.socket or not metaconcord.socket:isConnected() then return end -- metaconcord.print("Not connected.")
+	metaconcord.print("Disconnecting...")
+	metaconcord.socket:close()
 end
 
 function metaconcord.start()
-    if metaconcord.socket and metaconcord.socket:isConnected() then
-        local onDisconnected = metaconcord.socket.onDisconnected
+	if metaconcord.socket and metaconcord.socket:isConnected() then
+		local onDisconnected = metaconcord.socket.onDisconnected
 
-        metaconcord.socket.onDisconnected = function(self)
-            onDisconnected(self)
+		metaconcord.socket.onDisconnected = function(self)
+			onDisconnected(self)
 
-            timer.Simple(0, function()
-                metaconcord.connect()
-            end)
-        end
+			timer.Simple(0, function()
+				metaconcord.connect()
+			end)
+		end
 
-        metaconcord.stop()
-    else
-        metaconcord.connect()
-    end
+		metaconcord.stop()
+	else
+		metaconcord.connect()
+	end
+
+	timer.Create("metaconcordHeartbeat", 10, 0, function()
+		if metaconcord.socket and metaconcord.socket:isConnected() then
+			metaconcord.socket:write("") -- heartbeat LOL
+		else
+			metaconcord.print("Lost connection, reconnecting...")
+			metaconcord.start()
+		end
+	end)
 end
 
 function metaconcord.stop()
-    metaconcord.disconnect()
-    timer.Remove("metaconcordHeartbeat")
+	metaconcord.disconnect()
+	timer.Remove("metaconcordHeartbeat")
 end
 
 function metaconcord.getPayload(name)
-    for _, payload in next, metaconcord.payloads do
-        if name == payload.name then return payload end
-    end
+	for _, payload in next, metaconcord.payloads do
+		if name == payload.name then return payload end
+	end
 end
 
 hook.Add("Initialize", "metaconcord", metaconcord.start)
 
 if GAMEMODE then
-    hook.GetTable().Initialize.metaconcord()
+	hook.GetTable().Initialize.metaconcord()
 end
