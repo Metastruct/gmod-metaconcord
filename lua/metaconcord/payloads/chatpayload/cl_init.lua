4b55fb36c97369ac11755dee7d1ea0acc19b27a9
Python1320 <python1320+gh@gmail.com>
2021-01-11 01:17:53 +0200

    discord_postfix

--- lua/metaconcord/payloads/chatpayload/cl_init.lua
+++ lua/metaconcord/payloads/chatpayload/cl_init.lua
@@ -1,5 +1,6 @@
 local gray = Color(100, 100, 100)
 local blurple = Color(114, 137, 218)
+local discord_postfix = CreateClientConVar("discord_postfix","0",true)
 
 net.Receive("metaconcordChatPayload", function()
 	local name = net.ReadString()
@@ -15,6 +16,10 @@ net.Receive("metaconcordChatPayload", function()
 		-- Disgusting
 		roleColor = Color(tonumber("0x" .. hex:sub(1, 2)), tonumber("0x" .. hex:sub(3, 4)), tonumber("0x" .. hex:sub(5, 6)))
 	end
-
-	chat.AddText(gray, "[Discord] ", roleColor, name, color_white, ": ", content)
+	if discord_postfix:GetBool() then
+		chat.AddText(roleColor, name, color_white, ": ".. content,gray, " [D]")
+	else
+		chat.AddText(gray, "[Discord] ", roleColor, name, color_white, ": ", content)
+	end
+	
 end)
