0ce62acd15ace6c1c681cee1399a6bf31f0084b2
Techbot121 <techbot121@gmail.com>
2021-10-18 19:23:36 +0200

    forgot to remove this as well

--- lua/metaconcord/payloads/chatpayload/cl_init.lua
+++ lua/metaconcord/payloads/chatpayload/cl_init.lua
@@ -2,13 +2,11 @@ net.Receive("metaconcordChatPayload", function()
 	local name = net.ReadString()
 	local color = net.ReadInt(25)
 	local content = net.ReadString()
-	local roleColor = color > 1 and discord2color(color) or blurple
 	local avatar_url = net.ReadString()
 
 	local replied_name = net.ReadString()
 	local replied_color = net.ReadInt(25)
 	local replied_content = net.ReadString()
-	local replied_roleColor = replied_color > 1 and discord2color(replied_color) or blurple
 	local replied_avatar_url = net.ReadString()
 
 	hook.Run("DiscordSay", name, content, color, avatar_url, {
