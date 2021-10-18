05e34b9a12b5149ec9e117ee548e944a114f8119
Techbot121 <techbot121@gmail.com>
2021-10-18 20:15:12 +0200

    fix order

--- lua/metaconcord/payloads/chatpayload/cl_init.lua
+++ lua/metaconcord/payloads/chatpayload/cl_init.lua
@@ -1,13 +1,13 @@
 net.Receive("metaconcordChatPayload", function()
 	local name = net.ReadString()
 	local color = net.ReadInt(25)
-	local content = net.ReadString()
 	local avatar_url = net.ReadString()
+	local content = net.ReadString()
 
 	local replied_name = net.ReadString()
 	local replied_color = net.ReadInt(25)
-	local replied_content = net.ReadString()
 	local replied_avatar_url = net.ReadString()
+	local replied_content = net.ReadString()
 
 	hook.Run("DiscordSay", name, content, color, avatar_url, {
 		name = replied_name,
