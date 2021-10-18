c121ada08447bac2dbeb7fc81215f3eae2aab924
Techbot121 <techbot121@gmail.com>
2021-10-18 19:02:10 +0200

    add avatar payload support

--- lua/metaconcord/payloads/chatpayload/cl_init.lua
+++ lua/metaconcord/payloads/chatpayload/cl_init.lua
@@ -12,11 +12,13 @@ net.Receive("metaconcordChatPayload", function()
 	local color = net.ReadInt(25)
 	local content = net.ReadString()
 	local roleColor = color > 1 and discord2color(color) or blurple
+	local avatar_url = net.ReadString()
 
 	local replied_name = net.ReadString()
 	local replied_color = net.ReadInt(25)
 	local replied_content = net.ReadString()
 	local replied_roleColor = replied_color > 1 and discord2color(replied_color) or blurple
+	local replied_avatar_url = net.ReadString()
 
 	local ret = hook.Run("DiscordSay", name, content, {nick = replied_name, color = replied_color, content = replied_content})
 	if ret == false then return end
