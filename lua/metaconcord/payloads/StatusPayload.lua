00aeedc9880f48c7cf81a11d00fa99aa841213aa
Techbot121 <techbot121@gmail.com>
2022-06-15 21:47:50 +0200

    fix leaving not affecting status why was that there anyway?

--- lua/metaconcord/payloads/StatusPayload.lua
+++ lua/metaconcord/payloads/StatusPayload.lua
@@ -82,7 +82,6 @@ function StatusPayload:__call(socket)
 	end
 
 	local function remove(self, data)
-		if not connecting[data.userid] then return end
 		connecting[data.userid] = nil
 
 		timer.Simple(0, function()
