b7dc9695deebea24b8147dec2045b8deb3c1c86d
Techbot121 <techbot121@gmail.com>
2022-09-01 18:19:21 +0200

    support votekickfailed result

--- lua/metaconcord/payloads/VoteKickPayload.lua
+++ lua/metaconcord/payloads/VoteKickPayload.lua
@@ -5,76 +5,83 @@ VoteKickPayload.super = Payload
 VoteKickPayload.name = "VoteKickPayload"
 
 local function getPlayerNick(ply)
-  if type(ply) == "string" and ply:match("^STEAM_0:%d:%d*$") then
-	local _ply = player.GetBySteamID(ply)
+	if type(ply) == "string" and ply:match("^STEAM_0:%d:%d*$") then
+		local _ply = player.GetBySteamID(ply)
 
-	if IsValid(_ply) then
-	  ply = _ply
+		if IsValid(_ply) then
+		ply = _ply
+		end
 	end
-  end
 
-  if IsValid(ply) then
-	return ply:Nick()
-  elseif playerseen then
-	local seenEntry = playerseen.GetPlayerBySteamID(ply)
-	if seenEntry and seenEntry[1] then return seenEntry[1].nick or ply end
-  end
+	if IsValid(ply) then
+		return ply:Nick()
+		elseif playerseen then
+		local seenEntry = playerseen.GetPlayerBySteamID(ply)
+		if seenEntry and seenEntry[1] then return seenEntry[1].nick or ply end
+	end
 
-  return "???"
+	return "???"
 end
 
 function VoteKickPayload:__call(socket)
-  self.super.__call(self, socket)
+	self.super.__call(self, socket)
 
-  hook.Add("OnPlayerVoteKickRequested", self, function(_, offenderSteamID, reporterSteamID, reason)
-	self:write({
-	  offender = {
-		nick = getPlayerNick(offenderSteamID),
-		steamID = offenderSteamID,
-	  },
-	  reporter = {
-		nick = getPlayerNick(reporterSteamID),
-		steamID = reporterSteamID,
-	  },
-	  reason = reason
-	})
-  end)
-  hook.Add("OnPlayerVoteKicked", self, function(_, offenderSteamID, reporterSteamID, reason)
-	self:write({
-	  offender = {
-		nick = getPlayerNick(offenderSteamID),
-		steamID = offenderSteamID,
-	  },
-	  reporter = {
-		nick = getPlayerNick(reporterSteamID),
-		steamID = reporterSteamID,
-	  },
-	  reason = reason,
-	  success = true
-	})
-  end)
-  hook.Add("OnPlayerVoteKickFailed", self, function(_, offenderSteamID, reporterSteamID, reason, why)
-	self:write({
-	  offender = {
-		nick = getPlayerNick(offenderSteamID),
-		steamID = offenderSteamID,
-	  },
-	  reporter = {
-		nick = getPlayerNick(reporterSteamID),
-		steamID = reporterSteamID,
-	  },
-	  reason = reason,
-	  success = false
-	})
-  end)
+	hook.Add("OnPlayerVoteKickRequested", self, function(_, offenderSteamID, reporterSteamID, reason)
+		self:write({
+			offender = {
+			nick = getPlayerNick(offenderSteamID),
+			steamID = offenderSteamID,
+		},
+			reporter = {
+			nick = getPlayerNick(reporterSteamID),
+			steamID = reporterSteamID,
+		},
+			reason = reason
+		})
+	end)
 
-  return self
+	hook.Add("OnPlayerVoteKicked", self, function(_, offenderSteamID, reporterSteamID, reason)
+		self:write({
+			offender = {
+				nick = getPlayerNick(offenderSteamID),
+				steamID = offenderSteamID,
+			},
+			reporter = {
+				nick = getPlayerNick(reporterSteamID),
+				steamID = reporterSteamID,
+			},
+			reason = reason,
+			result = {
+				success = true
+			}
+		})
+	end)
+
+	hook.Add("OnPlayerVoteKickFailed", self, function(_, offenderSteamID, reporterSteamID, reason, why)
+		self:write({
+			offender = {
+				nick = getPlayerNick(offenderSteamID),
+				steamID = offenderSteamID,
+			},
+			reporter = {
+				nick = getPlayerNick(reporterSteamID),
+				steamID = reporterSteamID,
+			},
+			reason = reason,
+			result = {
+				success = false,
+				reason = why
+			}
+		})
+	end)
+
+	return self
 end
 
 function VoteKickPayload:__gc()
-  hook.Remove("OnPlayerVoteKickRequested", self)
-  hook.Remove("OnPlayerVoteKicked", self)
-  hook.Remove("OnPlayerVoteKickFailed", self)
+	hook.Remove("OnPlayerVoteKickRequested", self)
+	hook.Remove("OnPlayerVoteKicked", self)
+	hook.Remove("OnPlayerVoteKickFailed", self)
 end
 
 return setmetatable({}, VoteKickPayload)
